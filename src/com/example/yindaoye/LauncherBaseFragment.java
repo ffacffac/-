b337124874afde5e39f29b290f9458e880f54a4b
ffacffac <185138375@qq.com>
2016-03-08 12:22:56 +0800

    引导页，结合动画项目，第一次提交

---
+++ src/com/example/yindaoye/LauncherBaseFragment.java
@@ -0,0 +1,16 @@
+package com.example.yindaoye;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Fragment³éÏóÀà
+ * 
+ * @author ansen
+ * 
+ */
+public abstract class LauncherBaseFragment extends Fragment {
+    
+	public abstract void startAnimation();
+
+	public abstract void stopAnimation();
+}
